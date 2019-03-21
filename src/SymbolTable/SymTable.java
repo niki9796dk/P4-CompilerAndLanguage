f6328e358a4d0ca5dcd7e606c06441cd2938ab68
Big Guy <graae.christian@gmail.com>
2019-03-21 11:08:17 +0100

    SymbolType

---
+++ src/SymbolTable/SymTable.java
@@ -0,0 +1,14 @@
+package SymbolTable;
+
+public class SymTable implements Table {
+
+
+    public SymTable() {
+
+    }
+
+    @Override
+    public ScopeEntry getNewEntry() {
+        return null;
+    }
+}
--- src/SymbolTable/SymbolType.java
+++ src/SymbolTable/SymbolType.java
@@ -1,30 +1,38 @@
 package SymbolTable;
 
+import SymbolTable.Exceptions.NoSymbolParentException;
+
 public class SymbolType {
-    public static final SymbolType CHANNELIN = new SymbolType("channel:in","node");
-    public static final SymbolType CHANNELOUT = new SymbolType("channel:out","node");
-    public static final SymbolType SIZE = new SymbolType("size","non-node");
-    public static final SymbolType BLOCK = new SymbolType("block","node");
-    public static final SymbolType SOURCE = new SymbolType("source","node");
-    public static final SymbolType OPERATION = new SymbolType("operation","node");
+
     public static final SymbolType NODE = new SymbolType("node");
     public static final SymbolType NONNODE = new SymbolType("non-node");
 
+    public static final SymbolType CHANNELIN = new SymbolType("channel:in", NODE);
+    public static final SymbolType CHANNELOUT = new SymbolType("channel:out", NODE);
+    public static final SymbolType BLOCK = new SymbolType("block", NODE);
+    public static final SymbolType SOURCE = new SymbolType("source", NODE);
+    public static final SymbolType OPERATION = new SymbolType("operation", NODE);
+
+    public static final SymbolType SIZE = new SymbolType("size", NONNODE);
+
     //Make it a class hierachy so we can know both the specific type as well as the downcast?
     private String key;
-    private String parent;
+    private SymbolType parent;
+
     SymbolType(String key) {
         this.key = key.toLowerCase();
         parent = null;
     }
 
-    public SymbolType(String key, String parent) {
+    public SymbolType(String key, SymbolType parent) {
         this.key = key;
         this.parent = parent;
     }
 
-    private String getParent() {
-        return parent;
+    private SymbolType getParent() throws NoSymbolParentException {
+        if (parent != null)
+            return parent;
+        else throw new NoSymbolParentException("No parent for of symboltype \"" + this.getKey() + "\"");
     }
 
     public String getKey() {
