1770bed08bb02f2ba2ea1b692b60f563cb2cdf60
Big Guy <graae.christian@gmail.com>
2019-04-05 11:07:48 +0200

    Tests and cleanup

--- tests/SymbolTableImplementation/Enums/OperationTest.java
+++ tests/SymbolTableImplementation/Enums/OperationTest.java
@@ -8,5 +8,20 @@ class OperationTest {
 
     @Test
     void isValid() {
+        assertTrue(Operation.isValid("Addition"));
+        assertTrue(Operation.isValid("Multiplication"));
+        assertTrue(Operation.isValid("Subtraction"));
+        assertTrue(Operation.isValid("_Addition"));
+        assertTrue(Operation.isValid("_Multiplication"));
+        assertTrue(Operation.isValid("_Subtraction"));
+        assertTrue(Operation.isValid("_Division"));
+        assertTrue(Operation.isValid("_Sigmoid"));
+        assertTrue(Operation.isValid("_Tanh"));
+        assertTrue(Operation.isValid("_Relu"));
+        assertTrue(Operation.isValid("Transpose"));
+
+        assertFalse(Operation.isValid("jfd"));
+        assertFalse(Operation.isValid("lizard"));
+        assertFalse(Operation.isValid("Boop1i"));
     }
 }
