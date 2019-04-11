d4fc106016e8e30bcdbc18dae25fa575d10f2a57
Big Guy <graae.christian@gmail.com>
2019-04-11 09:42:02 +0200

    Javadocs

--- src/SymbolTableImplementation/BlockScope.java
+++ src/SymbolTableImplementation/BlockScope.java
@@ -4,6 +4,9 @@ import AST.Nodes.AbstractNodes.Nodes.AbstractNode;
 import AST.Nodes.AbstractNodes.Nodes.AbstractNodes.NumberedNodes.NamedNode;
 import Enums.AnsiColor;
 
+/**
+ *
+ */
 public class BlockScope {
     public static final String PROCEDURE_PREFIX = "PROC_";
     public static final String CHANNELS         = "ChannelDeclaration";
@@ -13,40 +16,70 @@ public class BlockScope {
     private NamedTable<Scope> scope;
     private AbstractNode node;
 
+    /**
+     * @param id The identifying ID of the block scope.
+     * @param node The node which
+     */
     public BlockScope(String id, AbstractNode node) {
         this.id = id;
         this.scope = new NamedTable<>();
         this.node = node;
     }
 
+    /**
+     * @return
+     */
     public String getId() {
         return id;
     }
 
+    /**
+     * @return
+     */
     public NamedTable<Scope> getScope() {
         return this.scope;
     }
 
+    /**
+     * @return
+     */
     public AbstractNode getNode() {
         return node;
     }
 
+    /**
+     * @param id
+     * @param node
+     */
     public void openScope(String id, NamedNode node) {
         this.getScope().setEntry(id, new Scope(id, node));
     }
 
+    /**
+     * @return
+     */
     public Scope getLatestSubScope() {
         return this.getScope().getLatest();
     }
 
+    /**
+     * @return
+     */
     public Scope getChannelDeclarationScope() {
         return this.getScope().getEntry(CHANNELS);
     }
 
+    /**
+     * @return
+     */
     public Scope getBlueprintScope() {
         return this.getScope().getEntry(BLUEPRINT);
     }
 
+    /**
+     * @param id
+     * @return
+     */
     public Scope getProcedureScope(String id) {
         return this.getScope().getEntry(PROCEDURE_PREFIX + id);
     }
--- src/SymbolTableImplementation/Scope.java
+++ src/SymbolTableImplementation/Scope.java
@@ -5,29 +5,49 @@ import AST.Nodes.AbstractNodes.Nodes.AbstractNodes.NumberedNodes.NamedNodes.Name
 import AST.Nodes.AbstractNodes.Nodes.AbstractNodes.NumberedNodes.NamedNode;
 import Enums.AnsiColor;
 
+/**
+ *
+ */
 public class Scope {
     private String id;
     private NamedTable<VariableEntry> scope;
     private AbstractNode node;
 
+    /**
+     * @param id
+     * @param node
+     */
     public Scope(String id, NamedNode node) {
         this.id = id;
         this.node = node;
         this.scope = new NamedTable<>();
     }
 
+    /**
+     * @return
+     */
     public String getId() {
         return id;
     }
 
+    /**
+     * @param node
+     */
     public void setVariable(NamedIdNode node) {
         this.scope.setEntry(node.getId(), new VariableEntry(node));
     }
 
+    /**
+     * @param id
+     * @return
+     */
     public VariableEntry getVariable(String id) {
         return this.scope.getEntry(id);
     }
 
+    /**
+     * @return
+     */
     public AbstractNode getNode() {
         return node;
     }
