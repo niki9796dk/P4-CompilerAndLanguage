b6eab8d0ac95ccc17017b54da14c9c88cfb25885
Big Guy <graae.christian@gmail.com>
2019-04-23 08:53:18 +0200

    Javadoc++

--- src/SymbolTableImplementation/Datastructures/HashMapLatest.java
+++ src/SymbolTableImplementation/Datastructures/HashMapLatest.java
@@ -13,9 +13,11 @@ import java.util.HashMap;
 public class HashMapLatest<K, V> extends HashMap<K, V> implements MapLatest<K, V> {
 
     /**
-     *
+     * Constructs an empty {@code HashMapLatest} with the default initial capacity
+     * (16) and the default load factor (0.75).
      */
     public HashMapLatest() {
+        super();
     }
 
     V latest = null;
