5bfa6c6f5af10eaa1fb375a4fea94c7d1ebe633d
Big Guy <graae.christian@gmail.com>
2019-04-23 08:55:25 +0200

    Javadoc++

--- src/SymbolTableImplementation/Datastructures/HashMapLatest.java
+++ src/SymbolTableImplementation/Datastructures/HashMapLatest.java
@@ -20,14 +20,35 @@ public class HashMapLatest<K, V> extends HashMap<K, V> implements MapLatest<K, V
         super();
     }
 
-    V latest = null;
 
+    /**
+     * The pointer to the value of the most recently added entry to the Map.
+     */
+    private V latest = null;
+
+    /**
+     * Associates the specified value with the specified key in this map.
+     * If the map previously contained a mapping for the key, the old
+     * value is replaced.
+     *
+     * Also changes the pointer to the most recently added value, to the value of the new entry put into the map.
+     *
+     * @param key key with which the specified value is to be associated
+     * @param value value to be associated with the specified key
+     * @return the previous value associated with {@code key}, or
+     *         {@code null} if there was no mapping for {@code key}.
+     *         (A {@code null} return can also indicate that the map
+     *         previously associated {@code null} with {@code key}.)
+     */
     @Override
     public V put(K key, V value) {
         this.latest = value;
         return super.put(key, value);
     }
 
+    /**
+     * @return The value of the most recently added entry, or {@code null} if this map contains no recently added elements.
+     */
     @Override
     public V getLatest() {
         return latest;
