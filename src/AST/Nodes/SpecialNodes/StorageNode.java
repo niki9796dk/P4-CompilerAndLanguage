f51c39a512e77b7adacf72c8347b641c1b552783
Big Guy <graae.christian@gmail.com>
2019-04-25 10:26:11 +0200

    Javadocs++

--- src/AST/Nodes/SpecialNodes/StorageNode.java
+++ src/AST/Nodes/SpecialNodes/StorageNode.java
@@ -2,21 +2,60 @@ package AST.Nodes.SpecialNodes;
 
 import AST.Nodes.AbstractNodes.Nodes.AbstractNode;
 
+/*
+* Objects of this class has several opportunities for misuse.
+* Notably:
+* * This should not be used as an actual node, and only as a return type. Yet an abstract syntax tree can be built using this node.
+* * * Suggested solution; Unknown
+* * The array of nodes is not immutable, despite lacking a setter.
+* * * Suggest solution; Get index data instead of the array directly. This is also cleaner code as it removes direct interaction with the internal structure.
+*
+*/
+
+/**
+ * Not a traditional node, but is instead used as a return type to return several nodes from method.
+ * Should not be used as a part of a built and final tree.
+ */
 public class StorageNode extends AbstractNode {
+
+    /**
+     * The nodes to use as a return value.
+     */
     private AbstractNode[] nodes;
+    /**
+     * The name of the node.
+     */
     private String name;
 
+    /**
+     * Construct a node without a name.
+     * @param nodes The list of nodes to allow getting.
+     */
     public StorageNode(AbstractNode ... nodes) {
         this("NoName", nodes);
     }
 
+    /**
+     * Construct a node with the specified name.
+     * @param name The name of the node.
+     * @param nodes The list of nodes to allow getting.
+     */
     public StorageNode(String name, AbstractNode ... nodes) {
         this.name = name;
         this.nodes = nodes;
     }
 
+    /**
+     * Get the nodes given to this node during its construction.
+     * @return The node contents.
+     */
     public AbstractNode[] getNodes() {
         return nodes;
     }
+
+    /**
+     * Get the name of the node.
+     * @return The name of the node.
+     */
     public String getName() { return this.name; }
 }
