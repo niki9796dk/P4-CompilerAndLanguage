b80f192b31cd3a5a02f71a761a8b99d2852c924d
niki9796dk <nikiewald@hotmail.dk>
2019-04-25 09:43:30 +0200

    Unfinished typeSystem

--- src/TypeChecker/TypeSystem.java
+++ src/TypeChecker/TypeSystem.java
@@ -3,7 +3,9 @@ package TypeChecker;
 import AST.Enums.NodeEnum;
 import AST.Nodes.AbstractNodes.Nodes.AbstractNode;
 import AST.Nodes.AbstractNodes.Nodes.AbstractNodes.NumberedNode;
+import AST.Nodes.AbstractNodes.Nodes.AbstractNodes.NumberedNodes.NamedNode;
 import AST.Nodes.AbstractNodes.Nodes.AbstractNodes.NumberedNodes.NamedNodes.NamedIdNode;
+import SymbolTableImplementation.BlockScope;
 import SymbolTableImplementation.SymbolTable;
 
 public class TypeSystem {
@@ -13,7 +15,7 @@ public class TypeSystem {
         this.symbolTable = symbolTable;
     }
 
-    public NodeEnum getTypeOfNode(AbstractNode node) {
+    public NodeEnum getTypeOfNode(AbstractNode node, String blockScopeId, String subScopeId) {
         NumberedNode numberedNode = (NumberedNode) node;
 
         switch (numberedNode.getNodeEnum()) {
@@ -39,12 +41,23 @@ public class TypeSystem {
 
             case DRAW:
                 return NodeEnum.BLUEPRINT_TYPE;
+
             case BUILD:
                 return NodeEnum.BLOCK_TYPE;
+
             case ASSIGN:
-                return this.getTypeOfNode(numberedNode.getChild());
+                return this.getTypeOfNode(numberedNode.getChild(), blockScopeId, subScopeId);
+
             case SELECTOR:
-                return null;
+                NamedIdNode namedIdNode = (NamedIdNode) node.getChild();
+
+                BlockScope blockScope = this.symbolTable.getBlockScope(blockScopeId);
+
+                return blockScope
+                        .getScope()
+                        .getEntry(subScopeId)
+                        .getVariable(namedIdNode.getId())
+                        .getSuperType();
 
             default:
                 throw new RuntimeException("Unexpected Node");
