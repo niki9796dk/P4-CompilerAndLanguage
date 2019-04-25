3e8d58cc52da07daef47e2f79feb0dcb601a81f6
Big Guy <graae.christian@gmail.com>
2019-04-25 09:30:43 +0200

    UnexpectedNodeException() added

--- src/AST/TreeWalks/SymbolTableVisitor.java
+++ src/AST/TreeWalks/SymbolTableVisitor.java
@@ -103,7 +103,7 @@ public class SymbolTableVisitor implements Visitor {
                 break;
 
             default:
-                throw new RuntimeException("Unexpected Node");
+                throw new UnexpectedNodeException(node);
         }
     }
 
