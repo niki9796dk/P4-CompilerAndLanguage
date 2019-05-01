fbdfece99152c248db4d8f4cbb9f84536aae37f2
Big Guy <graae.christian@gmail.com>
2019-05-01 14:21:35 +0200

    Binary Unitwise

---
+++ src/CodeGeneration/DataFlow/Operations/BinaryOperations/_Multiplication.java
@@ -0,0 +1,12 @@
+package CodeGeneration.DataFlow.Operations.BinaryOperations;
+
+import LinearAlgebra.Types.Matrices.Matrix;
+import LinearAlgebra.Types.Matrices.MatrixBuilder;
+
+public class _Multiplication extends BinaryMatrixOperation {
+    @Override
+    Matrix operation(Matrix in1, Matrix in2) {
+        MatrixBuilder out = new MatrixBuilder(in1.getRows(), in1.getColumns(), true);
+        return out.compLoop((r,c,v) -> out.setEntry(r,c, in1.getEntry(r,c) * in2.getEntry(r,c))).build();
+    }
+}
