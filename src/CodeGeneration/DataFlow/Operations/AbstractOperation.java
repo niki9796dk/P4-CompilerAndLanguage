83b0aea73a3ee887732796845e6da1bf23d8f39f
Big Guy <graae.christian@gmail.com>
2019-05-03 11:41:07 +0200

    isReady streamlining

--- src/CodeGeneration/DataFlow/Operations/AbstractOperation.java
+++ src/CodeGeneration/DataFlow/Operations/AbstractOperation.java
@@ -3,12 +3,12 @@ package CodeGeneration.DataFlow.Operations;
 import CodeGeneration.DataFlow.Network.AbstractBlock;
 import CodeGeneration.DataFlow.Network.Interfaces.Channel;
 import CodeGeneration.DataFlow.Network.Interfaces.Operation;
-import CodeGeneration.DataFlow.Network.Interfaces.SignalNode;
 import CodeGeneration.utility.Print;
 import Enums.AnsiColor;
 import LinearAlgebra.Types.Matrices.Matrix;
 
 public abstract class AbstractOperation extends AbstractBlock implements Operation {
+    private boolean isReady = false;
     protected Matrix result;
     protected Print print = new Print(AnsiColor.GREEN, "Operation." + this.getClass().getSimpleName());
 
@@ -24,33 +24,36 @@ public abstract class AbstractOperation extends AbstractBlock implements Operati
     }
 
     /**
-     * Given that all inputs of the operation are ready, perform operation and set outChannel signal to ready
+     * Given that all inputs of the operation are isReady, perform operation and set outChannel signal to isReady
      */
     @Override
     public void acceptReadySignal() {
         print.say("acceptReadySignal()");
-        if (this.isReady()) {// If all inputs are ready
+        if (this.isReady()) {// If all inputs are isReady
             this.performOperation();                // Then perform the operation
-            this.getOutputChannel().signalReady();  // And signal that the output channel now is ready.
+            this.getOutputChannel().signalReady();  // And signal that the output channel now is isReady.
         }
     }
 
     protected abstract Channel getOutputChannel();
 
     /**
-     * Checks if operation is ready to be performed
+     * Checks if operation is isReady to be performed
      *
-     * @return boolean value of whether all operation inputs are ready, or there are no inputs.
+     * @return boolean value of whether all operation inputs are isReady, or there are no inputs.
      */
     private boolean isReady() {
-        if(this.getInputs().isEmpty())
+        if(this.isReady)
             return true;
+
+        if(this.getInputs().isEmpty())
+            return (this.isReady = true);
 
         for (Channel inputChannel : this.getInputs())
             if(!inputChannel.isReady())
                 return false;
 
-        return true;
+        return (this.isReady = true);
     }
 
     /**
--- src/CodeGeneration/DataFlow/Operations/Nullary/NullaryAbstractOperation.java
+++ src/CodeGeneration/DataFlow/Operations/Nullary/NullaryAbstractOperation.java
@@ -57,4 +57,6 @@ public abstract class NullaryAbstractOperation extends AbstractOperation {
 
     }
     */
+
+
 }
