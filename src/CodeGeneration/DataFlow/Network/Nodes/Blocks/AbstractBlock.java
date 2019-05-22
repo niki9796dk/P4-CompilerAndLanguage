8489b9d6012c272a865f780e01fcf06e742b6279
xprovoke <mikela@outlook.dk>
2019-05-22 15:00:10 +0200

    Unit tests

--- src/CodeGeneration/DataFlow/Network/Nodes/Blocks/AbstractBlock.java
+++ src/CodeGeneration/DataFlow/Network/Nodes/Blocks/AbstractBlock.java
@@ -13,9 +13,14 @@ import LinearAlgebra.Types.Matrices.Matrix;
 import java.util.*;
 
 public abstract class AbstractBlock implements Block {
+
+    // Fields:
     private LinkedHashMap<String, Channel> inputChannels = new LinkedHashMap<>(2);
     private LinkedHashMap<String, Channel> outputChannels = new LinkedHashMap<>(1);
     public static BlockConfiguration configuration;
+
+    // Constants:
+    private static final double DEFAULT_LEARNING_RATE = 0.2;
 
     /**
      * Add an input channel
@@ -130,7 +135,7 @@ public abstract class AbstractBlock implements Block {
 
         myOutChannel.tether(targetChannel);
 
-        return null;
+        return this;
     }
 
     @Override
@@ -248,6 +253,6 @@ public abstract class AbstractBlock implements Block {
 
     @Override
     public void train(Matrix inputData, Matrix targetData, int iterations) {
-        this.train(inputData, targetData, iterations, 0.2);
+        this.train(inputData, targetData, iterations, DEFAULT_LEARNING_RATE);
     }
 }
---
+++ tests/CodeGeneration/DataFlow/Network/Nodes/BlocksAndSignalNodes/Operations/BinaryOperations/MatrixOperations/MultiplicationTest.java
@@ -0,0 +1,32 @@
+package CodeGeneration.DataFlow.Network.Nodes.BlocksAndSignalNodes.Operations.BinaryOperations.MatrixOperations;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MultiplicationTest {
+
+    private Multiplication multiplication;
+
+    @BeforeEach
+    void beforeEach() {
+
+    }
+
+    @Test
+    void operation() {
+    }
+
+    @Test
+    void operationBackpropagation() {
+    }
+
+    @Test
+    void calculateIn1Derivatives() {
+    }
+
+    @Test
+    void calculateIn2Derivatives() {
+    }
+}
