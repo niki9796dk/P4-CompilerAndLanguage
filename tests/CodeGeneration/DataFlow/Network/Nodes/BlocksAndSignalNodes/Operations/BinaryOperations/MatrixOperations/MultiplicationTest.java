fa7ea3733dd3fafe65cc32705965282b5e2384d7
kjulo <graae.christian@gmail.com>
2019-05-22 10:55:12 +0200

    Removed Input

--- tests/CodeGeneration/DataFlow/Network/Nodes/BlocksAndSignalNodes/Operations/BinaryOperations/MatrixOperations/MultiplicationTest.java
+++ tests/CodeGeneration/DataFlow/Network/Nodes/BlocksAndSignalNodes/Operations/BinaryOperations/MatrixOperations/MultiplicationTest.java
@@ -1,7 +1,6 @@
 package CodeGeneration.DataFlow.Network.Nodes.BlocksAndSignalNodes.Operations.BinaryOperations.MatrixOperations;
 
 import CodeGeneration.DataFlow.Network.Nodes.BlocksAndSignalNodes.Operation;
-import CodeGeneration.DataFlow.Network.Nodes.BlocksAndSignalNodes.Operations.NullaryOperation.Input;
 import CodeGeneration.DataFlow.Network.Nodes.BlocksAndSignalNodes.Operations.NullaryOperation.Source;
 import CodeGeneration.DataFlow.Network.Nodes.SignalNodes.BounceNode;
 import CodeGeneration.DataFlow.Network.Nodes.SignalNodes.BounceNodes.backpropagationBounce;
